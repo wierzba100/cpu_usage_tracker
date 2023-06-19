5ccbebacae324cae9e9b7b9b201898e515fcc81d
wierzba100 <85181137+wierzba100@users.noreply.github.com>
2023-06-19 20:58:48 +0200

    Add files via upload
    Test added

---
+++ test.py
@@ -0,0 +1,24 @@
+import signal
+import time
+import subprocess
+
+
+def sigterm_test():
+    cpu_usage_app = subprocess.Popen("./gpu_usage_tracker", stdout=subprocess.PIPE)
+
+    time.sleep(2)
+
+    cpu_usage_app.terminate()
+    cpu_usage_app.wait()
+
+    out = cpu_usage_app.stdout.read()
+    string_out = str(out)
+
+    if string_out[-17:] == "Program closed\\n'":
+        return 1
+    else:
+        return 0
+
+
+assert sigterm_test() == 1
+
