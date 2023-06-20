8e68a9d83386ce2df867d6bcbf5a902b5ddf445c
Filip wierzbinka <f.wierzbinka@gmail.com>
2023-06-20 22:05:22 +0200

    main for valgrind added

---
+++ main_valgrind.c
@@ -0,0 +1,61 @@
+#include "global_variables.h"
+#include "reader.h"
+#include "analyzer.h"
+#include "printer.h"
+#include "watchdog.h"
+#include "sigterm.h"
+#include "logger.h"
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t watchdog_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t readerCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t analyzerCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t watchdogCond = PTHREAD_COND_INITIALIZER;
+
+int Threads_Table[NR_OF_THREADS];
+volatile sig_atomic_t done = 0;
+long number_of_processors;
+
+#define SECONDS_TO_RUN 5
+
+void *terminate_after_N_sec(void* SecondsIn)
+{
+    int* Seconds = (int *) SecondsIn;
+    sleep(*Seconds);
+    term(0);
+    return NULL;
+}
+
+int main()
+{
+    CPUs_Data CPUs_Data={0};
+    number_of_processors = sysconf(_SC_NPROCESSORS_ONLN);
+    int nr_of_seconds = SECONDS_TO_RUN;
+
+    pthread_t thread0, thread1, thread2, thread3, thread4, thread5;
+
+    SIGTERM_init();
+
+    pthread_create(&thread0, NULL, reader, (void*) &CPUs_Data);
+    pthread_create(&thread1, NULL, analyzer, (void*) &CPUs_Data);
+    pthread_create(&thread2, NULL, printer, (void*) &CPUs_Data);
+    pthread_create(&thread3, NULL, watchdog, NULL);
+    pthread_create(&thread4, NULL, logger, (void*) &CPUs_Data);
+    pthread_create(&thread5, NULL, terminate_after_N_sec, (void*) &nr_of_seconds);
+
+    pthread_join(thread0, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+    pthread_join(thread5, NULL);
+
+    pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&watchdog_mutex);
+    pthread_cond_destroy(&readerCond);
+    pthread_cond_destroy(&analyzerCond);
+    pthread_cond_destroy(&watchdogCond);
+
+    printf("Program closed\n");
+    return 0;
+}
